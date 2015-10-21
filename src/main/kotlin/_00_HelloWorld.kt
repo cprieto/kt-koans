42c514dd8ea0ef027fdc2b32ce24f438c16f2b1e
Cristian Prieto <me@cprieto.com>
2015-10-21 17:04:39 +1100

    Initial commit with base layout

---
+++ src/main/kotlin/_00_HelloWorld.kt
@@ -0,0 +1,5 @@
+package i_introduction._0_HelloWorld.Hello
+
+fun task0() : String {
+    return "OK"
+}
---
+++ src/test/kotlin/_00_Start.kt
@@ -0,0 +1,11 @@
+package i_introduction._0_HelloWorld.Hello
+
+import org.testng.annotations.Test
+import kotlin.test.assertEquals
+
+class _00_Start {
+    @Test
+    fun testOk() {
+        assertEquals("OK", task0())
+    }
+}
