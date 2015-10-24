4ce28f6d75d3b7ef2bdd81d5b7f4d24342dcbc1f
Cristian Prieto <me@cprieto.com>
2015-10-25 09:07:59 +1100

    After reading the spec, simplified date comparison

--- src/ii_conventions/MyDate.kt
+++ src/ii_conventions/MyDate.kt
@@ -2,20 +2,9 @@ package ii_conventions
 
 data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
     override fun compareTo(other: MyDate): Int {
-        val yc = year.compareTo(other.year)
-        val mc = month.compareTo(other.month)
-        val dc = dayOfMonth.compareTo(other.dayOfMonth)
-
-        if (yc == -1) return -1
-        if (yc == 1) return 1
-
-        if (mc == -1) return -1
-        if (mc == 1) return 1
-
-        if (dc == -1) return -1
-        if (dc == 1) return 1
-
-        return 0
+        if (year != other.year) return year - other.year
+        if (month != other.month) return month - other.month
+        return dayOfMonth - other.dayOfMonth
     }
 }
 
