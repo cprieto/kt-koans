7abd339a3fa14ad63bbbd37d5da225b5e86f8acf
Cristian Prieto <me@cprieto.com>
2015-10-26 19:26:05 +1100

    Fixed test with floating point numbers

--- test/ii_collections/_19_Sum.kt
+++ test/ii_collections/_19_Sum.kt
@@ -4,9 +4,8 @@ import org.junit.Assert
 import org.junit.Test
 import ii_collections.data.*
 
-@Suppress("DEPRECATION")
 class _19_Sum {
     @Test fun testGetTotalOrderPrice() {
-        Assert.assertEquals(148.0, customers[nathan]!!.getTotalOrderPrice())
+        Assert.assertEquals(148.0, customers[nathan]!!.getTotalOrderPrice(), 1e-1)
     }
 }
